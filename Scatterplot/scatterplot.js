5a6fb84a687fba66ae4888a78fe260d05f2462fe
Mariette Souppe <msouppe@gmail.com>
2018-02-19 10:06:55 -0800

    Completed Scatter plot

--- Scatterplot/scatterplot.js
+++ Scatterplot/scatterplot.js
@@ -3,17 +3,6 @@ var margin = {left: 80, right: 80, top: 50, bottom: 50 },
     width = 960 - margin.left -margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// Define Color
-var colors = d3.scale.category20();
-
-// Define SVG
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 /*  Define scales
     Source: http://bl.ocks.org/weiglemc/6185069
         value accessor - returns the value to encode for a given data object.
@@ -24,23 +13,12 @@ var svg = d3.select("body")
 var xValue = function(d) { return d.gdp;}, // data -> value
     xScale = d3.scale.linear().range([0, width]), // value -> display
     xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(-height);
 
 var yValue = function(d) { return d.ecc;}, // data -> value
     yScale = d3.scale.linear().range([height, 0]), // value -> display
     yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
-    
-/*  Define Tooltip here
-    Source: http://bl.ocks.org/weiglemc/6185069
-*/
-var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-// Define Axis
-var xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickPadding(2);
-var yAxis = d3.svg.axis().scale(yScale).orient("left").tickPadding(2);
+    yAxis = d3.svg.axis().scale(yScale).orient("left").tickSize(-width);
 
 /*  Get Data
 */
@@ -55,115 +33,124 @@ d3.csv("scatterdata.csv", function(error, data) {
         d.population = +d.population;
         d.ecc = +d.ecc;
         d.ec = +d.ec;
-        console.log(d);
+        //console.log(d);
     });
-    
-    /*  Define domain for xScale and yScale 
-        Source: http://bl.ocks.org/weiglemc/6185069
-            Don't want dots overlapping axis, so add in buffer to data domain
-    */
-    xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-    yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
 
-    //Draw Scatterplot
-    svg.selectAll(".dot")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "dot")
-        .attr("r", function(d) { return Math.sqrt(d.ec)/.2; })
-        .attr("cx", xMap)
-        .attr("cy", yMap)
-        .style("fill", function (d) { return colors(d.country); })
-        
-        /*  Add .on("mouseover", .....
-            Add Tooltip.html with transition and style 
-            Source: http://bl.ocks.org/weiglemc/6185069
-        */      
-        .on("mouseover", function(d) {
-          tooltip.transition()
-               .duration(200)
-               .style("opacity", .9);
-          tooltip.html(
-                d.country + "<br/> Population: " + d.population + " Million <br/> GDP: $" + d.gdp + "Trillion <br/> EPC: " + d.ecc + " Million BTUs <br/> Total: " + d.ec + " Trillion BTUs")
-                .style("left", (d3.event.pageX + 5) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
+    var xMax = d3.max(data, function(d) { return d.gdp; }) * 1.05,
+        xMin = d3.min(data, function(d) { return d.gdp; }),
+        xMin = xMin > 0 ? 0 : xMin,
+        yMax = d3.max(data, function(d) { return d.ecc; }) * 1.05,
+        yMin = d3.min(data, function(d) { return d.ecc; }),
+        yMin = yMin > 0 ? 0 : yMin;
+
+    /* Define Domain */
+    xScale.domain([xMin, xMax]);
+    yScale.domain([yMin, yMax]);
+
+    /* Define Color */
+    var colors = d3.scale.category20();
     
-        /* Add .on("mouseout", .... */
-        .on("mouseout", function(d) {
-          tooltip.transition()
-               .duration(500)
-               .style("opacity", 0);
+    /* Define tool tip */
+    var tip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-10, 0])
+        .html(function(d) {
+            return d.country + "<br/> Population: " + d.population + " million <br/> GDP: $" + d.gdp + " trillion <br/> EPC: " + d.ecc + " million BTUs <br/> Total: " + d.ec + " trillion BTUs" ;
         });
     
+    /* Define zoom scale and behavior */
+    var zoomBeh = d3.behavior.zoom()
+        .x(xScale)
+        .y(yScale)
+        .scaleExtent([0, 500])
+        .on("zoom", zoom);
     
-    // Scale Changes as we Zoom
-    // Call the function d3.behavior.zoom to Add zoom
+    
+    var svg = d3.select("#scatter")
+        .append("svg")
+            .attr("width", 960)
+            .attr("height", 500)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(zoomBeh);
 
-    // Draw Country Names
-    svg.selectAll(".text")
-        .data(data)
-        .enter().append("text")
-        .attr("class","text")
-        .style("text-anchor", "start")
-        .attr("x", function(d) {return xScale(d.gdp);})
-        .attr("y", 100) //function(d) {return yScale(d.epc);}
-        .style("fill", "black")
-        .text(function (d) {return d.name; });
+    svg.call(tip);
 
-    // X-Axis
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .append("text")
-        .attr("class", "label")
-        .attr("y", 50)
-        .attr("x", width/2)
-        .style("text-anchor", "middle")
-        .attr("font-size", "12px")
-        .text("GDP (in Trillion US Dollars) in 2010");
-    
-    // Y-Axis
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("class", "label")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -50)
-        .attr("x", -50)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .attr("font-size", "12px")
-        .text("Energy Consumption per Capita (in Million BTUs per person)");
-    
-    // Draw legend colored rectangles
     svg.append("rect")
-        .attr("x", width-250)
-        .attr("y", height-190)
-        .attr("width", 220)
-        .attr("height", 180)
-        .attr("fill", "lightgrey")
-        .style("stroke-size", "1px");
+        .attr("width", width)
+        .attr("height", height);
 
+    svg.append("g")
+            .classed("x axis", true)
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+        .append("text")
+            .classed("label", true)
+            .attr("x", width/2 + 100)
+            .attr("y", 50)
+            .style("text-anchor", "end")
+            .text("GDP (in Trillions of US Dollars) in 2010");
+    
+    svg.append("g")
+            .classed("y axis", true)
+            .call(yAxis)
+        .append("text")
+            .classed("label", true)
+            .attr("transform", "rotate(-90)")
+            .attr("x", -50)
+            .attr("y", -50)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Energy Consumptionper Capita (in MillionBTUs per Person ");
+
+    var objects = svg.append("svg")
+        .classed("objects", true)
+        .attr("width", width)
+        .attr("height", height);
+    
+    objects.append("svg:line")
+        .classed("axisLine hAxisLine", true)
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", width)
+        .attr("y2", 0)
+        .attr("transform", "translate(0," + height + ")");
+
+    objects.append("svg:line")
+        .classed("axisLine vAxisLine", true)
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", 0)
+        .attr("y2", height);
+
+    objects.selectAll(".dot")
+            .data(data)
+        .enter().append("circle")
+            .classed("dot", true)
+            .attr("r", function (d) { return Math.sqrt(d.ec)/.4; })
+            .attr("transform", transform)
+            .style("fill", function(d) { return colors(d.country); })
+            .on("mouseover", tip.show)
+            .on("mouseout", tip.hide);
+    
+    /* Legend */
     svg.append("circle")
         .attr("r", 5)
         .attr("cx", width-100)
         .attr("cy", height-175)
-        .style("fill", "white");
+        .style("fill", "green");
     
     svg.append("circle")
         .attr("r", 15.8)
         .attr("cx", width-100)
         .attr("cy", height-150)
-        .style("fill", "white");
+        .style("fill", "green");
 
     svg.append("circle")
-        .attr("r", 50)
+        .attr("r", 48)
         .attr("cx", width-100)
         .attr("cy", height-80)
-        .style("fill", "white");
+        .style("fill", "green");
 
     svg.append("text")
         .attr("class", "label")
@@ -174,7 +161,7 @@ d3.csv("scatterdata.csv", function(error, data) {
 
     svg.append("text")
         .attr("class", "label")
-        .attr("x", width -150)
+        .attr("x", width-150)
         .attr("y", height-147)
         .style("text-anchor", "end")
         .text(" 10 Trillion BTUs");
@@ -193,7 +180,34 @@ d3.csv("scatterdata.csv", function(error, data) {
         .style("text-anchor", "middle")
         .style("fill", "Green") 
         .attr("font-size", "16px")
-        .text("Total Energy Consumption");     
+        .text("Total Energy Consumption");
 
+    /* Click button fuction to reset the axis */
+    d3.select("input").on("click", change);
     
-}); // d3.csv
+    function change() {
+        xMax = d3.max(data, function(d) { return d.gdp; });
+        xMin = d3.min(data, function(d) { return d.gdp; });
+
+        zoomBeh.x(xScale.domain([xMin, xMax])).y(yScale.domain([yMin, yMax]));
+
+        var svg = d3.select("#scatter").transition();
+        svg.select(".x.axis").duration(750).call(xAxis).select(".label").text("GDP (in Trillions of US Dollars) in 2010");
+
+        objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
+    }
+
+    function zoom() {
+        svg.select(".x.axis").call(xAxis);
+        svg.select(".y.axis").call(yAxis);
+
+        svg.selectAll(".dot")
+            .attr("transform", transform);
+    }
+
+    function transform(d) {
+        return "translate(" + xScale(d.gdp) + "," + yScale(d.ecc) + ")";
+    }
+    
+
+}); // d3.csv    
